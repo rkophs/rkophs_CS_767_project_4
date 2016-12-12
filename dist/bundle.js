d998f7383e746510a61e4ecec626a5104c0569ef
Ryan Kophs <rkophs@gmail.com>
2016-12-12 15:50:04 -0500

    complete

--- dist/bundle.js
+++ dist/bundle.js
@@ -50050,11 +50050,11 @@
 
 	var _immutable2 = _interopRequireDefault(_immutable);
 
-	var _LinePlot = __webpack_require__(501);
+	var _LinePlot = __webpack_require__(502);
 
 	var _LinePlot2 = _interopRequireDefault(_LinePlot);
 
-	var _d = __webpack_require__(502);
+	var _d = __webpack_require__(501);
 
 	var d3 = _interopRequireWildcard(_d);
 
@@ -50613,7 +50613,7 @@
 
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 
-	var _d = __webpack_require__(502);
+	var _d = __webpack_require__(501);
 
 	var d3 = _interopRequireWildcard(_d);
 
@@ -50766,119 +50766,6 @@
 
 /***/ },
 /* 501 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/*
-	* @Author: ryan
-	* @Date:   2016-11-29 21:50:35
-	* @Last Modified by:   Ryan Kophs
-	* @Last Modified time: 2016-11-30 09:59:18
-	*/
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-		value: true
-	});
-
-	var _d = __webpack_require__(502);
-
-	var d3 = _interopRequireWildcard(_d);
-
-	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-	var LinePlot = function LinePlot() {
-
-		var type = function type(d, _, columns) {
-			d.date = parseTime(d.date);
-			for (var i = 1, n = columns.length, c; i < n; ++i) {
-				d[c = columns[i]] = +d[c];
-			}return d;
-		};
-
-		var svg = d3.select("#chart_svg"),
-		    margin = { top: 20, right: 80, bottom: 30, left: 50 },
-		    width = 960 - margin.left - margin.right,
-		    height = 500 - margin.top - margin.bottom,
-		    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-		var x = d3.scaleLinear().range([0, width]),
-		    y = d3.scaleLinear().range([height, 0]),
-		    z = d3.scaleOrdinal(d3.schemeCategory10);
-
-		var line = d3.line().curve(d3.curveBasis).x(function (d) {
-			return x(d.date);
-		}).y(function (d) {
-			return y(d.temperature);
-		});
-
-		var cities = [{
-			id: "New York",
-			values: [{
-				date: 20111001,
-				temperature: 63
-			}, {
-				date: 20111002,
-				temperature: 65
-			}, {
-				date: 20111003,
-				temperature: 66
-			}]
-		}, {
-			id: "Austin",
-			values: [{
-				date: 20111001,
-				temperature: 93
-			}, {
-				date: 20111002,
-				temperature: 95
-			}, {
-				date: 20111003,
-				temperature: 96
-			}]
-		}];
-
-		x.domain([20111001, 20111003]);
-
-		y.domain([d3.min(cities, function (c) {
-			return d3.min(c.values, function (d) {
-				return d.temperature;
-			});
-		}), d3.max(cities, function (c) {
-			return d3.max(c.values, function (d) {
-				return d.temperature;
-			});
-		})]);
-
-		z.domain(cities.map(function (c) {
-			return c.id;
-		}));
-
-		g.append("g").attr("class", "axis axis--x").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)).append("text").attr("x", 50).attr("dx", "0.71em").attr("fill", "#000").text("Current (Amps)");
-
-		g.append("g").attr("class", "axis axis--y").call(d3.axisLeft(y)).append("text").attr("transform", "rotate(-90)").attr("y", 10).attr("dy", "0.71em").attr("fill", "#000").text("Potential (V)");
-
-		var city = g.selectAll(".city").data(cities).enter().append("g").attr("class", "city");
-
-		city.append("path").attr("class", "line").attr("d", function (d) {
-			return line(d.values);
-		}).style("stroke", function (d) {
-			return z(d.id);
-		});
-
-		city.append("text").datum(function (d) {
-			return { id: d.id, value: d.values[d.values.length - 1] };
-		}).attr("transform", function (d) {
-			return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")";
-		}).attr("x", 3).attr("dy", "0.35em").style("font", "10px sans-serif").text(function (d) {
-			return d.id;
-		});
-	};
-
-	exports.default = LinePlot;
-
-/***/ },
-/* 502 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// https://d3js.org Version 4.4.0. Copyright 2016 Mike Bostock.
@@ -67275,6 +67162,119 @@
 
 	})));
 
+
+/***/ },
+/* 502 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/*
+	* @Author: ryan
+	* @Date:   2016-11-29 21:50:35
+	* @Last Modified by:   Ryan Kophs
+	* @Last Modified time: 2016-11-30 09:59:18
+	*/
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+
+	var _d = __webpack_require__(501);
+
+	var d3 = _interopRequireWildcard(_d);
+
+	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+	var LinePlot = function LinePlot() {
+
+		var type = function type(d, _, columns) {
+			d.date = parseTime(d.date);
+			for (var i = 1, n = columns.length, c; i < n; ++i) {
+				d[c = columns[i]] = +d[c];
+			}return d;
+		};
+
+		var svg = d3.select("#chart_svg"),
+		    margin = { top: 20, right: 80, bottom: 30, left: 50 },
+		    width = 960 - margin.left - margin.right,
+		    height = 500 - margin.top - margin.bottom,
+		    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		var x = d3.scaleLinear().range([0, width]),
+		    y = d3.scaleLinear().range([height, 0]),
+		    z = d3.scaleOrdinal(d3.schemeCategory10);
+
+		var line = d3.line().curve(d3.curveBasis).x(function (d) {
+			return x(d.date);
+		}).y(function (d) {
+			return y(d.temperature);
+		});
+
+		var cities = [{
+			id: "New York",
+			values: [{
+				date: 20111001,
+				temperature: 63
+			}, {
+				date: 20111002,
+				temperature: 65
+			}, {
+				date: 20111003,
+				temperature: 66
+			}]
+		}, {
+			id: "Austin",
+			values: [{
+				date: 20111001,
+				temperature: 93
+			}, {
+				date: 20111002,
+				temperature: 95
+			}, {
+				date: 20111003,
+				temperature: 96
+			}]
+		}];
+
+		x.domain([20111001, 20111003]);
+
+		y.domain([d3.min(cities, function (c) {
+			return d3.min(c.values, function (d) {
+				return d.temperature;
+			});
+		}), d3.max(cities, function (c) {
+			return d3.max(c.values, function (d) {
+				return d.temperature;
+			});
+		})]);
+
+		z.domain(cities.map(function (c) {
+			return c.id;
+		}));
+
+		g.append("g").attr("class", "axis axis--x").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)).append("text").attr("x", 50).attr("dx", "0.71em").attr("fill", "#000").text("Current (Amps)");
+
+		g.append("g").attr("class", "axis axis--y").call(d3.axisLeft(y)).append("text").attr("transform", "rotate(-90)").attr("y", 10).attr("dy", "0.71em").attr("fill", "#000").text("Potential (V)");
+
+		var city = g.selectAll(".city").data(cities).enter().append("g").attr("class", "city");
+
+		city.append("path").attr("class", "line").attr("d", function (d) {
+			return line(d.values);
+		}).style("stroke", function (d) {
+			return z(d.id);
+		});
+
+		city.append("text").datum(function (d) {
+			return { id: d.id, value: d.values[d.values.length - 1] };
+		}).attr("transform", function (d) {
+			return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")";
+		}).attr("x", 3).attr("dy", "0.35em").style("font", "10px sans-serif").text(function (d) {
+			return d.id;
+		});
+	};
+
+	exports.default = LinePlot;
 
 /***/ },
 /* 503 */
